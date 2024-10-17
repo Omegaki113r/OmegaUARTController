170c63e6fd80c5ad36d465f1b8b1edd985aa7f26
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-10-17 15:44:33 +0530

    initial code written

---
+++ inc/OmegaUARTController.h
@@ -0,0 +1,45 @@
+/**
+ * @file OmegaUARTController.h
+ * @author Omegaki113r
+ * @date Thursday, 17th October 2024 3:33:52 pm
+ * @copyright Copyright 2024 - 2024 0m3g4ki113r, Xtronic
+ * */
+/*
+ * Project: OmegaUARTController
+ * File Name: OmegaUARTController.h
+ * File Created: Thursday, 17th October 2024 3:33:52 pm
+ * Author: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Last Modified: Thursday, 17th October 2024 3:37:28 pm
+ * Modified By: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Copyright 2024 - 2024 0m3g4ki113r, Xtronic
+ * -----
+ * HISTORY:
+ * Date      	By	Comments
+ * ----------	---	---------------------------------------------------------
+ */
+
+#ifndef __OMEGA_UART_CONTROLLER_H__
+#define __OMEGA_UART_CONTROLLER_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdint.h>
+
+    typedef uint64_t OmegaUARTHandle;
+
+    typedef struct
+    {
+        void (*read_uart)(uint8_t *, size_t, uint32_t);
+    } OmegaUARTController_t;
+
+    OmegaUARTHandle OmegaUARTController_init(OmegaUARTController_t *);
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __OMEGA_UART_CONTROLLER_H__
---
+++ src/OmegaUARTController.c
@@ -0,0 +1,27 @@
+/**
+ * @file OmegaUARTController.c
+ * @author Omegaki113r
+ * @date Thursday, 17th October 2024 3:34:02 pm
+ * @copyright Copyright 2024 - 2024 0m3g4ki113r, Xtronic
+ * */
+/*
+ * Project: OmegaUARTController
+ * File Name: OmegaUARTController.c
+ * File Created: Thursday, 17th October 2024 3:34:02 pm
+ * Author: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Last Modified: Thursday, 17th October 2024 3:38:52 pm
+ * Modified By: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Copyright 2024 - 2024 0m3g4ki113r, Xtronic
+ * -----
+ * HISTORY:
+ * Date      	By	Comments
+ * ----------	---	---------------------------------------------------------
+ */
+
+#include "OmegaUARTController.h"
+
+OmegaUARTHandle OmegaUARTController_init(OmegaUARTController_t *controller)
+{
+}
