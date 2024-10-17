d17bafbbb784d4b7ff22ecab139291f641de8cdd
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-10-17 19:48:58 +0530

    size_t definition location

--- inc/OmegaUARTController.h
+++ inc/OmegaUARTController.h
@@ -10,7 +10,7 @@
  * File Created: Thursday, 17th October 2024 3:33:52 pm
  * Author: Omegaki113r (omegaki113r@gmail.com)
  * -----
- * Last Modified: Thursday, 17th October 2024 7:00:21 pm
+ * Last Modified: Thursday, 17th October 2024 7:48:45 pm
  * Modified By: Omegaki113r (omegaki113r@gmail.com)
  * -----
  * Copyright 2024 - 2024 0m3g4ki113r, Xtronic
@@ -28,6 +28,7 @@ extern "C"
 {
 #endif
 
+#include <stddef.h>
 #include <stdint.h>
 
     typedef enum
